a3b526b092b2eaca3c47a8dd0c78506a6a41a9a4
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2024-11-30 23:10:21 -0600

    Add create trips

--- ActivityTracker/Controllers/ActivityController.cs
+++ ActivityTracker/Controllers/ActivityController.cs
@@ -5,8 +5,31 @@ namespace ActivityTracker.Controllers;
 
 public class ActivityController : Controller
 {
+    private readonly AppDbContext _context;
+    
+    public ActivityController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
     public IActionResult Index()
     {
+        var activities = _context.Activities.ToList();
+        return View(activities);
+    }
+    
+    public IActionResult Create()
+    {
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create(ATActivity activity)
+    {
+        if (!ModelState.IsValid) return View(activity);
+        activity.Date = DateTime.Now;
+        _context.Activities.Add(activity);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
